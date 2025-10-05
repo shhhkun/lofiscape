44f0513605fc8aaf31f75f9948a7604e581531c5
Serjo <serjobarron@gmail.com>
2025-10-04 23:20:57 -0700

    added bg & changed card placements & appearance

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,21 +13,23 @@ const AmbianceSounds = [
 export default function HomePage() {
   return (
     <div>
-      <main className="flex min-h-dvh items-center justify-center">
-        <div
-          className="flex flex-col items-center p-6 rounded-lg gap-6"
-          style={{ backgroundColor: "var(--card-bg)" }}
-        >
-          <h1 className="text-4xl font-extrabold">Create Your Focus Scape</h1>
+      <main className="flex flex-col min-h-dvh bg-[url('/bg.jpg')] bg-cover bg-top justify-between">
 
-          <div className="flex flex-row flex-wrap gap-4">
-            {AmbianceSounds.map((sound) => (
-              <AmbianceButton
-                key={sound.name}
-                ambiance={sound.name}
-                audioUrl={sound.fileUrl}
-              />
-            ))}
+        <div className="flex justify-center bg-[var(--card-bg)] backdrop-blur-xs p-6 my-6 mx-auto rounded-lg">
+          <h1 className="text-4xl font-extrabold">Create Your Focus Scape</h1>
+        </div>
+
+        <div className="flex w-full justify-center">
+          <div className="flex bg-[var(--card-bg)] backdrop-blur-xs py-6 px-24 m-6 rounded-lg gap-6">
+            <div className="flex flex-row flex-wrap gap-4">
+              {AmbianceSounds.map((sound) => (
+                <AmbianceButton
+                  key={sound.name}
+                  ambiance={sound.name}
+                  audioUrl={sound.fileUrl}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </main>
