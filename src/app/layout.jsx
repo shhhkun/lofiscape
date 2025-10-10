719953ba0595d64ba7052aa8f282308bae153f7b
Serjo <serjobarron@gmail.com>
2025-10-10 12:10:11 -0700

    added og (opengraph) to page metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,21 @@ export const metadata = {
   title: "Lo-Fi Scape",
   description:
     "Curate your perfect productivity envrionment. Mix calming LoFi beats with custom ambient soundscapes, from gentle rain to a lively cafe.",
+  openGraph: {
+    title: "Lo-Fi Scape: Minimal Mixer for Focus",
+    description:
+      "Curate your perfect productivity envrionment. Mix calming LoFi beats with custom ambient soundscapes, from gentle rain to a lively cafe.",
+    url: "https://lofiscape.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "https://lofiscape.vercel.app/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Lo-Fi Scape Preview",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
